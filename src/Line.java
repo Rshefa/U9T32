fe04c66845e7bfe9e899876f9da65c65bed04cd1
Rshefa <raisas@nycstudents.net>
2023-03-10 11:01:05 -0500

    Initial commit

---
+++ src/Line.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+
+public class Line extends Shape {
+
+    public  Line(){
+        super();
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // set the color to draw the shape in
+        g.setColor(getColor());
+
+
+        g.drawLine(getP1().x, getP1().y, getP2().x, getP2().y ); // the getter methods here are INHERITED FROM SHAPE!
+    }
+
+
+}
